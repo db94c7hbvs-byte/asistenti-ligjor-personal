9895bb53cb2a1d6fc35148feab5bdb8e6334b67f
db94c7hbvs-byte <db94c7hbvs@privaterelay.appleid.com>
2026-04-28 19:31:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chains import create_retrieval_chain
+from langchain-classic import create_retrieval_chain
 from langchain_chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
